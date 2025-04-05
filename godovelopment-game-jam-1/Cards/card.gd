235afe8e98d0680fbb99886edf90f22badd8bd60
MamaDespik <40304336+MamaDespik@users.noreply.github.com>
2025-04-05 10:37:36 -0400

    Card Flipping

--- godovelopment-game-jam-1/Cards/card.gd
+++ godovelopment-game-jam-1/Cards/card.gd
@@ -1,5 +1,31 @@
-extends Node3D
+extends Node2D
+class_name Card
 
-func _process(_delta: float) -> void:
-	rotation.y += PI/80
+@export var card_title:String
+@export var card_image:Texture
+@export var card_text:String
+
+var is_face_up:bool = true
+
+@onready var card_model = $CardSprite/CardModelViewport/CardModel
+@onready var card_front = %CardFront
+
+func _ready():
+	card_front.title_label.text = card_title
+	card_front.image_texture.texture = card_image
+	card_front.text_label.text = card_text
+	return
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"): #DEBUG
+		flip()
+	return
+
+func flip():
+	var target_angle:float
+	if is_face_up: target_angle = PI
+	else: target_angle = 0
+	var tween = create_tween()
+	tween.tween_property(card_model, "rotation", Vector3(0,target_angle,0), .1)
+	tween.tween_property(self, "is_face_up", !is_face_up, 0)
 	return
