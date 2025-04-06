fe6cc1dc928fd7b1e317a46ba7197ffe66d3e4e4
MamaDespik <40304336+MamaDespik@users.noreply.github.com>
2025-04-05 22:33:19 -0400

    Tweak Card Flip Animation Speed

--- godovelopment-game-jam-1/Cards/card.gd
+++ godovelopment-game-jam-1/Cards/card.gd
@@ -17,7 +17,7 @@ func _ready():
 	card_front.image_texture.texture = card_image
 	card_front.text_label.text = card_text
 	return
-	
+
 func _input(event):
 	if event.is_action_pressed("ui_accept"): #DEBUG
 		flip()
@@ -28,10 +28,10 @@ func flip():
 	if is_face_up: target_angle = PI
 	else: target_angle = 0
 	var tween = create_tween()
-	tween.tween_property(card_model, "rotation", Vector3(0,target_angle,0), .1)
+	tween.tween_property(card_model, "rotation", Vector3(0,target_angle,0), .3)
 	tween.tween_property(self, "is_face_up", !is_face_up, 0)
 	return
-	
+
 func use():
 	used.emit(self)
 	return
