eafe3afc6d8bb7acd2a3ccad82a38404742c549b
Derrick Brown <mamadespik@gmail.com>
2025-04-14 13:01:59 -0400

    Renaming and Cost-Checking for Card Removal

--- godovelopment-game-jam-1/Levels/shop_room.gd
+++ godovelopment-game-jam-1/Levels/shop_room.gd
@@ -2,19 +2,19 @@ extends Room
 class_name ShopRoom
 
 @export var relic_scenes:Array[PackedScene]
-@export var discard_cost:int = 5
+@export var removal_cost:int = 5
 
 var ready_to_exit:bool = false
-var ready_to_discard:bool = false
-var discarding:bool = false
+var ready_to_remove:bool = false
+var removing:bool = false
 var deck:CardPile
 
 @onready var exit_label: Label = $ShopExit/ExitLabel
-@onready var discard_label: Label = $CardDiscardZone/DiscardLabel
+@onready var remove_card_label: Label = $CardRemovalZone/RemoveCardLabel
 @onready var pedestal_container: Node2D = $PedestalContainer
 @onready var relic_1_pedistal: ItemPedestal = $PedestalContainer/Relic1Pedistal
 @onready var relic_2_pedistal: ItemPedestal = $PedestalContainer/Relic2Pedistal
-@onready var hand: Hand = $CardDiscardZone/Hand
+@onready var hand: Hand = $CardRemovalZone/Hand
 
 func _ready() -> void:
 	relic_1_pedistal.item_scene = relic_scenes.pick_random()
@@ -34,15 +34,8 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("select"):
 		if ready_to_exit:
 			room_cleared.emit()
-		if discarding:
-			if player.gold_count < discard_cost:
-				get_viewport().set_input_as_handled()
-				print("Can't afford discard.")
-			else:
-				player.gold_count -= discard_cost
-				print("Charged for discard.")
-		elif ready_to_discard:
-			start_discard()
+		if ready_to_remove:
+			start_card_removal()
 	return
 
 func set_player():
@@ -50,17 +43,18 @@ func set_player():
 		item_pedestal.player = player
 	return
 
-func start_discard():
-	discarding = true
-	#TODO display prompt to discard for cost
+func start_card_removal():
+	removing = true
+	#TODO display prompt to remove for cost
 	for i in deck.cards.size():
 		var card:Card = deck.draw()
 		card.simulated = true
 		hand.add_card(card)
 	return
 
-func stop_discard():
-	discarding = false
+func stop_card_removal():
+	removing = false
+	#TODO hide prompt to remove for cost
 	var cards_to_remove:Array[Card]
 	for unpicked_card:Card in hand.cards:
 		unpicked_card.simulated = false
@@ -79,21 +73,24 @@ func _on_exit_player_detector_body_exited(_body: Node2D) -> void:
 	exit_label.hide()
 	return
 
-func _on_discard_player_detector_body_entered(_body: Node2D) -> void:
-	ready_to_discard = true
-	discard_label.show()
+func _on_removal_player_detector_body_entered(_body: Node2D) -> void:
+	ready_to_remove = true
+	remove_card_label.show()
 	return
 
-func _on_discard_player_detector_body_exited(_body: Node2D) -> void:
-	ready_to_discard = false
-	discard_label.hide()
-	stop_discard()
+func _on_removal_player_detector_body_exited(_body: Node2D) -> void:
+	ready_to_remove = false
+	remove_card_label.hide()
+	stop_card_removal()
 	return
 
 func _on_hand_card_removed(card:Card):
-	if discarding:
-		card.queue_free()
+	if removing:
+		if player.gold_count >= removal_cost:
+			player.gold_count -= removal_cost
+			card.queue_free()
+		else:
+			hand.add_card(card)
 	else:
-
 		deck.add_card(card)
 	return
