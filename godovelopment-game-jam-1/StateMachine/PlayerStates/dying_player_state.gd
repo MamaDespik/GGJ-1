0b70e3f4b1fea5b3cf25bbb0d4bbc746d5896163
Derrick Brown <mamadespik@gmail.com>
2025-04-15 16:44:32 -0400

    Walking Up and Dying Animations

--- godovelopment-game-jam-1/StateMachine/PlayerStates/dying_player_state.gd
+++ godovelopment-game-jam-1/StateMachine/PlayerStates/dying_player_state.gd
@@ -5,7 +5,7 @@ var player_parent:Player
 
 func enter():
 	player_parent = parent
-	print("Player Died")
+	player_parent.start_animation("dying")
 	player_parent.stop_invincibility()
 	player_parent.hurt_box.set_deferred("enabled", false)
 	#TODO
