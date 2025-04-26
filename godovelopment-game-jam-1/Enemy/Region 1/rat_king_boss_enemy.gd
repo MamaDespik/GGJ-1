cb0a8d6637be79ee4bd9dbf62ab1cd8c3ff82430
MamaDespik <40304336+MamaDespik@users.noreply.github.com>
2025-04-26 10:47:30 -0400

    Rat King Boss Enemy

---
+++ godovelopment-game-jam-1/Enemy/Region 1/rat_king_boss_enemy.gd
@@ -0,0 +1,46 @@
+extends BossEnemy
+
+@export var rat_enemy_scene:PackedScene
+
+var rats:Array[Enemy]
+var minimum_spawn_time:float = .5
+var maximum_spawn_time:float = 3
+
+@onready var rat_spawn_timer = $RatSpawnTimer
+
+func _ready():
+	super()
+	rat_spawn_timer.wait_time = maximum_spawn_time
+	rat_spawn_timer.start()
+	return
+
+func get_target():
+	var x_target:float = randf_range(0,1920)
+	var y_target:float = randf_range(0,1080)
+	target = Vector2(x_target,y_target)
+	return
+
+func spawn_rat():
+	var new_rat:Enemy = rat_enemy_scene.instantiate()
+	rats.append(new_rat)
+	new_rat.player = player
+	new_rat.paused = false
+	add_child(new_rat)
+	new_rat.top_level = true
+	new_rat.position = global_position
+	return
+
+
+func _on_died(_enemy):
+	for rat:Enemy in rats:
+		if is_instance_valid(rat): 
+			rat.health_module.take_damage(999)
+	return
+
+
+func _on_rat_spawn_timer_timeout():
+	spawn_rat()
+	var health_ratio:float = float(health_module.current_health)/float(health_module.max_health)
+	rat_spawn_timer.wait_time = lerp(minimum_spawn_time, maximum_spawn_time, health_ratio)
+	rat_spawn_timer.start()
+	return
--- godovelopment-game-jam-1/Enemy/boss_enemy.gd
+++ godovelopment-game-jam-1/Enemy/boss_enemy.gd
@@ -1,4 +1,5 @@
 extends Enemy
+class_name BossEnemy
 
 var boss_health_module_scene:PackedScene = preload("res://Modules/boss_health_module.tscn")
 
@@ -7,7 +8,6 @@ func _ready() -> void:
 	health_module.queue_free()
 	health_module = boss_health_module_scene.instantiate()
 	health_module.max_health = max_health
-	#health_module.current_health = max_health
 	add_child(health_module)
 	super()
 	return
