af0e2f530e6fca84433e85cf0741fe1fb59f95cc
MamaDespik <40304336+MamaDespik@users.noreply.github.com>
2025-05-01 08:33:23 -0400

    Dash SFX and Animation Tweak

--- godovelopment-game-jam-1/Card Actions/Archer/dash_card_action.gd
+++ godovelopment-game-jam-1/Card Actions/Archer/dash_card_action.gd
@@ -1,5 +1,7 @@
 extends CardAction
 
+var player_animation_speed
+
 var speed_boost:float = 2000
 
 func start_invulnerability():
@@ -8,10 +10,13 @@ func start_invulnerability():
 	boost_vector = boost_vector.rotated(global_rotation)
 	player.velocity += boost_vector
 	player.boosting = true
+	player_animation_speed = player.animation_player.speed_scale
+	player.animation_player.speed_scale = 3
 	return
 
 func stop_invulnerability():
 	player.hurt_box.enabled = true
 	player.boosting = false
+	player.animation_player.speed_scale = player_animation_speed
 	queue_free()
 	return
