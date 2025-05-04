55939fe808beff39c58507e46463ea13a44a32e7
MamaDespik <40304336+MamaDespik@users.noreply.github.com>
2025-05-04 13:31:53 -0400

    MiniBoss and Boss Differences

--- godovelopment-game-jam-1/Levels/region.gd
+++ godovelopment-game-jam-1/Levels/region.gd
@@ -4,10 +4,14 @@ class_name Region
 @export var floor_lengths:Array[int]
 @export var floor_scene:PackedScene
 @export var shop_floor_scene:PackedScene
-
+@export var boss_floor_scene:PackedScene
 
 func get_floor() -> Floor:
-	var new_floor:Floor = floor_scene.instantiate()
+	var new_floor:Floor
+	if floor_lengths.size() > 1:
+		new_floor = floor_scene.instantiate()
+	if floor_lengths.size() == 1:
+		new_floor = boss_floor_scene.instantiate()
 	new_floor.critical_length = floor_lengths.pop_front()
 	return new_floor
 
